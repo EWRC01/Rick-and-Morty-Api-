2c4d52df8560eab415f61780344625cd01214b84
@Wilfredo <edwin1026@hotmail.es>
2022-10-01 15:26:37 -0600

    Rick and Morty Api Consume with React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Formulario from "./components/Formulario";
+import PintarPersonajes from "./components/PintarPersonajes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [nombre, setNombre] = useState("");
+
+    return (
+        <div className="container">
+            <h1>APP</h1>
+            <Formulario setNombre={setNombre} />
+            <button
+                className="btn btn-success mt-2"
+                onClick={() => setNombre("")}
+            >
+                Reiniciar
+            </button>
+            <PintarPersonajes nombre={nombre} />
+        </div>
+    );
+};
 
 export default App;
